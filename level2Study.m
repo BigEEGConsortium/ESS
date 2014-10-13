f8790e3d21c19c3444043b1ed420b40563a69aba
Nima Bigdely-Shamlo <nimabg@bitbucket.org>
2014-10-12 23:25:20 -0700

    Added valiation for channel labels in . Level 2 resume half implememted. Now level 1 data can be read from Level 2 embedded node.

--- level2Study.m
+++ level2Study.m
@@ -1,7 +1,7 @@
 classdef level2Study
     % Be careful! any properties placed below will be written in the XML
     % file.
-    properties         
+    properties
         % version of STDL2 used. Mandatory.
         studyLevel2SchemaVersion = ' ';
         
@@ -10,11 +10,11 @@ classdef level2Study
         
         % a unique identifier (uuid) with 32 random  alphanumeric characters and four hyphens).
         % It is used to uniquely identify each STDL2 document.
-        uuid = ' ';   
+        uuid = ' ';
         
         % the URI pointing to the root folder of associated data folder. If the XML file is located
-        % in the default root folder, this should be ‘.’ (current directory). If for example the data files   
-        % and the root folder are placed on a remote FTP location, <rootURI> should be set to  
+        % in the default root folder, this should be ‘.’ (current directory). If for example the data files
+        % and the root folder are placed on a remote FTP location, <rootURI> should be set to
         % ‘ftp://domain.com/study’. The concatenation or <rootURI> and <filename> for each file
         % should always produce a valid, downloadable URI.adable URI.
         rootURI = '.';
@@ -26,7 +26,7 @@ classdef level2Study
         % runs.
         filters = struct('filter', struct('filterLabel', ' ', 'executionOrder', ' ', ...
             'softwareEnvironment', ' ', 'softwarePackage', ' ', 'functionName', ' ',...
-            'parameters', struct('parameter', struct('name', ' ', 'value', ' ')), 'recordingParemeterSetLabel', ' ')); 
+            'parameters', struct('parameter', struct('name', ' ', 'value', ' ')), 'recordingParemeterSetLabel', ' '));
         
         % files containing EEGLAB datasets, each recording gets its own studyLevel2 file
         % (we do not combine datasets).
@@ -51,7 +51,7 @@ classdef level2Study
         
         % Level 1 study contains basic information about study and raw data files.
         % It is created based on level1XmlFilePath input parameter
-        level1StudyObj            
+        level1StudyObj
         
         % ESS-convention level 2 folder where all level 2 data are
         % organized during level 1 -> 2 conversion using the pipeline.
@@ -71,16 +71,23 @@ classdef level2Study
             end;
             
             
-            inputOptions = arg_define(1,varargin, ...
+            inputOptions = arg_define(0,varargin, ...
                 arg('level1XmlFilePath', '','','ESS Standard Level 1 XML Filename.', 'type', 'char'), ...
-                arg('level2XmlFilePath', '','','ESS Standard Level 2 XML Filename.', 'type', 'char'), ...             
+                arg('level2XmlFilePath', '','','ESS Standard Level 2 XML Filename.', 'type', 'char'), ...
                 arg('createNewFile', false,[],'Always create a new file. Forces the creation of a new (partially empty, filled according to input parameters) ESS file. Use with caution since this forces an un-promted overwrite if an ESS file already exists in the specified path.', 'type', 'cellstr') ...
                 );
             
-            if nargin > 0
+            obj.level2XmlFilePath = inputOptions.level2XmlFilePath;
+            
+            if ~isempty(obj.level2XmlFilePath)
+                obj = obj.read;
+            end;
+            
+            if ~isempty(inputOptions.level1XmlFilePath)
                 obj.level1StudyObj = level1Study(inputOptions.level1XmlFilePath);
                 obj.level1XmlFilePath = inputOptions.level1XmlFilePath;
             end;
+            
         end;
         
         function obj = write(obj, level2XmlFilePath)
@@ -103,7 +110,7 @@ classdef level2Study
             % include level 1 xml in studyLevel1 field.
             xmlAsStructure.studyLevel1 = xml_read (obj.level1XmlFilePath);
             
-            % prevent xml_ioi from adding extra 'Item' fields and write the XML 
+            % prevent xml_ioi from adding extra 'Item' fields and write the XML
             Pref.StructItem = false;
             Pref.CellItem = false;
             xml_write(obj.level2XmlFilePath, xmlAsStructure, 'studyLevel2', Pref);
@@ -115,10 +122,23 @@ classdef level2Study
             names = fieldnames(xmlAsStructure);
             
             for i=1:length(names)
-                obj.(names{i}) = xmlAsStructure.(names{i});
+                if strcmp(names{i}, 'studyLevel1')
+                    % load the level 1 data into its own object instead of
+                    % a regular structure field under level 2
+                    
+                    % prevent xml_ioi from adding extra 'Item' fields and write the XML
+                    Pref.StructItem = false;
+                    Pref.CellItem = false;
+                    temporaryLevel1XmlFilePath = [tempname '.xml'];
+                    xml_write(temporaryLevel1XmlFilePath, xmlAsStructure.studyLevel1, 'studyLevel1', Pref);
+                    
+                    obj.level1StudyObj = level1Study(temporaryLevel1XmlFilePath);
+                else
+                    obj.(names{i}) = xmlAsStructure.(names{i});
+                end;
             end;
             
-            %% TODO: convert integer values 
+            %% TODO: convert integer values
             
             % the assignment above is quite raw as it does not check for the
             % consistency of inner values with deeper structures
@@ -129,6 +149,22 @@ classdef level2Study
         function obj = createLevel2Study(obj, level2Folder, varargin)
             obj.level2Folder = level2Folder;
             
+            % start from index 1 if the first studyLevel2File is pactically empty,
+            % otherwise start after the last studyLevel2File
+            if length(obj.studyLevel2Files.studyLevel2File) == 1 && isempty(strtrim(obj.studyLevel2Files.studyLevel2File(1).studyLevel2FileName))
+                studyLevel2FileCounter = 1;
+            else
+                studyLevel2FileCounter = 1 + length(obj.studyLevel2Files.studyLevel2File);
+            end;
+            
+            alreadyProcessedDataRecordingUuid = {};
+            for i=1:length(obj.studyLevel2Files.studyLevel2File)
+                recordingUuid = strtrim(obj.studyLevel2Files.dataRecordingUuid);
+                if ~isempty(uuid)
+                    alreadyProcessedDataRecordingUuid{end+1} = recordingUuid;
+                end;
+            end;
+            
             % make top folders
             mkdir(level2Folder);
             mkdir([level2Folder filesep 'session']);
@@ -137,68 +173,77 @@ classdef level2Study
             % process each session before moving to the other
             for i=1:length(obj.level1StudyObj.sessionTaskInfo)
                 for j=1:length(obj.level1StudyObj.sessionTaskInfo(i).dataRecording)
-                    fileNameFromObj = obj.level1StudyObj.sessionTaskInfo(i).dataRecording(j).filename;
-                    
-                    % read data
-                    
-                    % copy and rename the data recording files
-                    level1FileFolder = fileparts(obj.level1XmlFilePath);
-                    
-                    if isempty(obj.rootURI)
-                        rootFolder = level1FileFolder;
-                    elseif obj.rootURI(1) == '.'
-                        rootFolder = [level1FileFolder filesep obj.rootURI(2:end)];
+                    % do not processed data recordings that have already
+                    % been processed.
+                    if ~ismember(obj.level1StudyObj.sessionTaskInfo(i).dataRecording(j).dataRecordingUuid, alreadyProcessedDataRecordingUuid)
+                        fileNameFromObj = obj.level1StudyObj.sessionTaskInfo(i).dataRecording(j).filename;
+                        
+                        % read data
+                        level1FileFolder = fileparts(obj.level1XmlFilePath);
+                        
+                        if isempty(obj.rootURI)
+                            rootFolder = level1FileFolder;
+                        elseif obj.rootURI(1) == '.'
+                            rootFolder = [level1FileFolder filesep obj.rootURI(2:end)];
+                        end;
+                        
+                        % search for the file both next to the xml file and in the standard ESS
+                        % convention location
+                        nextToXMLFilePath = [rootFolder filesep fileNameFromObj];
+                        fullEssFilePath = [rootFolder filesep 'session' filesep obj.level1StudyObj.sessionTaskInfo(i).sessionNumber filesep fileNameFromObj];
+                        
+                        if ~isempty(fileNameFromObj) && exist(fullEssFilePath, 'file')
+                            fileFinalPath = fullEssFilePath;
+                        elseif ~isempty(fileNameFromObj) && exist(nextToXMLFilePath, 'file')
+                            fileFinalPath = nextToXMLFilePath;
+                        elseif ~isempty(fileNameFromObj) % when the file is specified but cannot be found on disk
+                            fileFinalPath = [];
+                            fprintf('File specified for data recoding %d of sesion number %s does not exist, \r         i.e. cannot find either %s or %s.\n', j, obj.sessionTaskInfo(i).sessionNumber, nextToXMLFilePath, fullEssFilePath);
+                            fprintf('You might want to run validate() routine.\n');
+                        else % the file name is empty
+                            fileFinalPath = [];
+                            fprintf('You have not specified any file for data recoding %d of sesion number %s\n', j, obj.sessionTaskInfo(i).sessionNumber);
+                            fprintf('You might want to run validate() routine.\n');
+                        end;
+                        
+                        % run the pipeline
+                        EEG = eeg_emptyset;
+                        
+                        % lets assume we got EEG variable
+                        % write data
+                        sessionFolder = [level2Folder filesep 'session' filesep obj.level1StudyObj.sessionTaskInfo(i).sessionNumber];
+                        if ~exist(sessionFolder, 'dir')
+                            mkdir(sessionFolder);
+                        end;
+                        
+                        % if recording file name matches ESS Level 1 convention
+                        % then just modify it a bit to conform to level2
+                        [path name ext] = fileparts(fileFinalPath);
+                        % see if the file name is already in ESS
+                        % format, hence no name change is necessary
+                        subjectInSessionNumber = obj.level1StudyObj.sessionTaskInfo(i).subject(j).inSessionNumber;
+                        itMatches = level1Study.fileNameMatchesEssConvention([name ext], 'eeg', obj.level1StudyObj.studyTitle, obj.level1StudyObj.sessionTaskInfo(i).sessionNumber,...
+                            subjectInSessionNumber, obj.level1StudyObj.sessionTaskInfo(i).taskLabel, j);
+                        
+                        if itMatches
+                            % change the eeg_ at the beginning to
+                            % eeg_studyLevel2_
+                            filenameInEss = ['eeg_studyLevel2_' name(5:end) ext];
+                        else % convert to ess convention
+                            filenameInEss = obj.essConventionFileName('eeg', ['studyLevel2_' obj.level1StudyObj.studyTitle], obj.level1StudyObj.sessionTaskInfo(i).sessionNumber,...
+                                subjectInSessionNumber, obj.level1StudyObj.sessionTaskInfo(i).taskLabel, j, name, extension);
+                        end;
+                        
+                        pop_saveset(EEG, 'filename', filenameInEss, 'filepath', sessionFolder, 'savemode', 'onefile', 'version', '7.3');
+                        
+                        obj.studyLevel2Files.studyLevel2File(studyLevel2FileCounter).studyLevel2FileName = filenameInEss;
+                        obj.studyLevel2Files.studyLevel2File(studyLevel2FileCounter).dataRecordingUuid = obj.level1StudyObj.sessionTaskInfo(i).dataRecording(j).dataRecordingUuid;
+                        
+                        studyLevel2FileCounter = studyLevel2FileCounter + 1;
+                        obj.write([level2Folder filesep 'studyLevel2_description.xml']);
                     end;
-                    
-                    % search for the file both next to the xml file and in the standard ESS
-                    % convention location
-                    nextToXMLFilePath = [rootFolder filesep fileNameFromObj];
-                    fullEssFilePath = [rootFolder filesep 'session' filesep obj.sessionTaskInfo(i).sessionNumber filesep fileNameFromObj];
-                    
-                    if ~isempty(fileNameFromObj) && exist(fullEssFilePath, 'file')
-                        fileFinalPath = fullEssFilePath;
-                    elseif ~isempty(fileNameFromObj) && exist(nextToXMLFilePath, 'file')
-                        fileFinalPath = nextToXMLFilePath;
-                    elseif ~isempty(fileNameFromObj) % when the file is specified but cannot be found on disk
-                        fileFinalPath = [];
-                        fprintf('File specified for data recoding %d of sesion number %s does not exist, \r         i.e. cannot find either %s or %s.\n', j, obj.sessionTaskInfo(i).sessionNumber, nextToXMLFilePath, fullEssFilePath);
-                        fprintf('You might want to run validate() routine.\n');
-                    else % the file name is empty
-                        fileFinalPath = [];
-                        fprintf('You have not specified any file for data recoding %d of sesion number %s\n', j, obj.sessionTaskInfo(i).sessionNumber);
-                        fprintf('You might want to run validate() routine.\n');
-                    end;
-                    
-                    % run the pipeline
-                    
-                    % lets assume we got EEG variable
-                    % write data
-                    sessionFolder = [level2Folder filesep 'session' filesep obj.level1StudyObj.sessionTaskInfo(i).sessionNumber];
-                    if ~exist(sessionFolder, 'dir')
-                        mkdir(sessionFolder);
-                    end;
-                    
-                    % if recording file name matches ESS Level 1 convention
-                    % then just modify it a bit to conform to level2
-                    [path name ext] = fileparts(fileFinalPath);
-                    % see if the file name is already in ESS
-                    % format, hence no name change is necessary
-                    itMatches = level1Study.fileNameMatchesEssConvention([name ext], 'eeg', obj.level1StudyObj.studyTitle, obj.sessionTaskInfo(i).sessionNumber,...
-                        subjectInSessionNumber, obj.sessionTaskInfo(i).taskLabel, j);
-                                        
-                    if itMatches
-                        % change the eeg_ at the beginning to
-                        % eeg_studyLevel2_
-                        filenameInEss = [eeg_studyLevel2_ name(5:end) ext];
-                    else % convert to ess convention
-                        filenameInEss = obj.essConventionFileName('eeg', ['studyLevel2_' obj.studyTitle], obj.sessionTaskInfo(i).sessionNumber,...
-                            subjectInSessionNumber, obj.sessionTaskInfo(i).taskLabel, j, name, extension);
-                    end;
-                    
                 end;
             end;
-            
-            obj.write('studyLevel2_description.xml');
         end;
     end;
 end
