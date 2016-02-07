f44b19b7152c1b0022ad031ad3a9a83510fa5a21
Nima Bigdely-Shamlo <nimabg@gmail.com>
2016-02-06 16:08:36 -0800

    finished level 1 json-based report

--- Ess_tools/asset/json_report/make_report.js
+++ Ess_tools/asset/json_report/make_report.js
@@ -38,9 +38,13 @@ function textFromItemsAndCounts(itemStrings, itemCounts, quantityString){
 	return text;
 }
 
-// ---------------------------------------------------------------------
+function isAvailable(stringValue) {
+	return stringValue != 'NA' || stringValue != '-' || stringValue != ' ';
+}
 
-level1Study = study.parentStudyObj.level1StudyObj;
+// -----------------------------------------------------------------------------
+
+var level1Study = study.parentStudyObj.level1StudyObj;
 
 // make key variables read from JSON to all be arrays.
 level1Study.sessionTaskInfo = makeIntoArray(level1Study.sessionTaskInfo);
@@ -52,15 +56,15 @@ for (var i=0; i < level1Study.sessionTaskInfo.length; i++) {
 level1Study.recordingParameterSet = makeIntoArray(level1Study.recordingParameterSet);
 for (var k=0; k < level1Study.recordingParameterSet.length; k++){
 	level1Study.recordingParameterSet[k].modality = makeIntoArray(level1Study.recordingParameterSet[k].modality);
-	for (var k=0; k < level1Study.recordingParameterSet.length; k++){
-
-	}
 }
 
-extracted = {};
+var extracted = {}; // data extracted from study JSON object and sent to AngularJS (as $Scope)
 extracted.shortDescription = level1Study.studyShortDescription;
 extracted.title = study.title;
 extracted.fullDescription = level1Study.studyDescription;
+extracted.showLevel1Notice = true;
+extracted.showLevel2Notice = false;
+extracted.showLevel3Notice = false;
 
 extracted.numberOfSessions = level1Study.sessionTaskInfo.length;
 
@@ -114,7 +118,6 @@ for (var i=0; i < level1Study.sessionTaskInfo.length; i++){
 		dataRecording.push({
 			'sessionNumber': level1Study.sessionTaskInfo[i].sessionNumber,
 			'taskLabel': level1Study.sessionTaskInfo[i].taskLabel,
-			'purpose': level1Study.sessionTaskInfo[i].taskLabel,
 			'sessionLabId': level1Study.sessionTaskInfo[i].labId,
 			'channelLocationsFilename': level1Study.sessionTaskInfo[i].subject[0].channelLocations,
 			'subjectGroup': level1Study.sessionTaskInfo[i].subject[0].group,
@@ -190,20 +193,46 @@ for (var i=0; i < level1Study.experimentersInfo.length; i++){
 		extracted.experimenters.push(text);
 	}
 }
+
+// point of contact
+// show the section if it is specified.
+extracted.showlevel1PointOfContact = level1Study.contactInfo.email != '' || level1Study.contactInfo.email != '-' || level1Study.contactInfo.email != '-';
+extracted.level1PointOfContact = '';
+if (isAvailable(level1Study.contactInfo.name)){
+	extracted.level1PointOfContact = level1Study.contactInfo.name;
+}
+
+if (extracted.level1PointOfContact !=''){
+	extracted.level1PointOfContact += ', ';
+}
+extracted.level1PointOfContact += 'Email: ' + level1Study.contactInfo.email;
+
+if (extracted.level1PointOfContact !=''){
+	extracted.level1PointOfContact += ', ';
+}
+if (extracted.level1PointOfContact !='' && isAvailable(level1Study.contactInfo.phone)){
+	extracted.level1PointOfContact += 'Phone: ' + level1Study.contactInfo.phone;
+}
+
+
 // hide publications section when informationis not available
 extracted.showExperimenters = extracted.experimenters.length != 1 || extracted.experimenters[0] != '';
+extracted.rootURI = level1Study.rootURI;
+if (extracted.rootURI == '.') // make it proper relative link
+	extracted.rootURI = '..';
 
-var columnDefs = [
+var level1DataRecordingsColumnDefs = [
 	{headerName: "Session", field: "sessionNumber", minWidth: 100, width:100, pinned: true, checkboxSelection: false},
 	{headerName: "Task", field: "taskLabel", minWidth: 100, width:100, pinned: true},
-	{headerName: "Purpose", field: "purpose", minWidth: 100, width:100},
 	{headerName: "EEG Sampling Rate (Hz)", field: "eegSamplingFrequency", minWidth: 200, width:200},
 	{headerName: "Modalities (number of channels)", field: "modalitiesAndTheirChannelsText", minWidth: 250, width:250},
 	{headerName: "Session Lab Id", field: "sessionLabId", minWidth: 150, width:150},
 	{headerName: "Notes", field: "notes", minWidth: 100, width:100},
-	{headerName: "Filename", field: "filename", minWidth: 100, width:300},
+	{headerName: "Filename", field: "filename", minWidth: 300, width:300,
+	template: '<a href="' + extracted.rootURI + '/session/{{data.sessionNumber}}/{{data.filename}}""><span ng-bind="data.filename"></span></a>'},
 	{headerName: "Original Filename", field: "originalFileNameAndPath", minWidth: 100, width:300},
-	{headerName: "Channel Locations", field: "channelLocationsFilename", minWidth: 100, width:300},
+	{headerName: "Channel Locations", field: "channelLocationsFilename", minWidth: 100, width:300,
+template: '<a href="' + extracted.rootURI + '/session/{{data.sessionNumber}}/{{data.channelLocationsFilename}}""><span ng-bind="data.channelLocationsFilename"></span></a>'},
 	{headerName: "Subject", cellStyle:{textAlign: 'center', color: 'red'},  children: [
 		{headerName: "Group", field: "subjectGroup", minWidth: 80, width:80},
 		{headerName: "Gender", field: "subjectGender", minWidth: 100, width:100},
@@ -214,39 +243,143 @@ var columnDefs = [
 	]},
 ];
 
-var rowData = dataRecording;
 
 function autoSizeAllGridColumns() {
 	var allColumnIds = [];
-	columnDefs.forEach( function(columnDef) {
+	level1DataRecordingsColumnDefs.forEach( function(columnDef) {
 		allColumnIds.push(columnDef.field);
 		if (columnDef.children)
 		for (var i = 0; i < columnDef.children.length; i++) {
 			allColumnIds.push(columnDef.children[i].field);
 		}
 	});
-	extracted.gridOptions.columnApi.autoSizeColumns(allColumnIds);
+	extracted.level1DataRecordingsGridOptions.columnApi.autoSizeColumns(allColumnIds);
 }
 
-function whenGridIsReady (event){
+function whenLevel1DataRecordingsGridIsReady (event){
 	autoSizeAllGridColumns();
+	// do it again in 1 second so Angular ha time to dynamically render all filenames
+	setTimeout(autoSizeAllGridColumns, 1000);
 }
 
-extracted.gridOptions = {
-	columnDefs: columnDefs,
-	rowData: rowData,
+extracted.level1DataRecordingsGridOptions = {
+	columnDefs: level1DataRecordingsColumnDefs,
+	rowData: dataRecording,
 	enableColResize: true,
 	enableSorting: true,
-//	unSortIcon: true,
+	//	unSortIcon: true,
 	enableFilter: true,
-	onReady: whenGridIsReady
+	suppressMenuHide: true,
+	angularCompileRows: true, // for template
+	onReady: whenLevel1DataRecordingsGridIsReady
 };
-
-
-function onFilterChanged(value) {
-    extracted.gridOptions.api.setQuickFilter(value);
+// without this if there is only one row it becomes hidden.
+if (extracted.level1DataRecordingsGridOptions.rowData.length == 1){
+	extracted.level1DataRecordingsGridOptions.rowHeight = 50;
 }
 
+
+// run the grid filter only when there was not a chage of value right before it.
+// this is to prevent stuttering.
+
+var lastTimeLevel1DatarecordingsFilterChanged = new Date();
+
+function filterLevel1DataRecordingsIfNoChange(value) {
+	var thisTime = new Date();
+	if (thisTime.getTime() - lastTimeLevel1DatarecordingsFilterChanged.getTime() > 200){
+		extracted.level1DataRecordingsGridOptions.api.setQuickFilter(value);
+	}
+}
+
+function onLevel1DataRecordingsFilterChanged(value) {
+	lastTimeLevel1DatarecordingsFilterChanged = new Date();
+	setTimeout(filterLevel1DataRecordingsIfNoChange, 300, value);
+}
+
+// task grid
+var level1TasksColumnDefs = [
+	{headerName: "Task Label", field: "taskLabel", minWidth: 200, width:200},
+	{headerName: "Description", field: "description", minWidth: 400, width:400},
+	{headerName: "HED Tags", field: "tag", minWidth: 400, width:400}
+];
+
+
+extracted.level1TasksGridOptions = {
+	columnDefs: level1TasksColumnDefs,
+	rowData:makeIntoArray(level1Study.tasksInfo),
+	enableColResize: true,
+	enableSorting: true,
+	enableFilter: true,
+};
+// without this if there is only one row it becomes hidden.
+if (extracted.level1TasksGridOptions.rowData.length == 1){
+	extracted.level1TasksGridOptions.rowHeight = 50;
+}
+
+// Events grid
+var level1EventsColumnDefs = [
+	{headerName: "Event Code", field: "code", minWidth: 200, width:200, pinned: true},
+	{headerName: "Label", field: "label", minWidth: 400, width:400},
+	{headerName: "Description", field: "description", minWidth: 200, width:500, cellStyle: {'white-space': 'pre-wrap'}},
+	{headerName: "HED Tags", field: "tag", minWidth:2300, width:600,cellStyle: {'white-space': 'pre-wrap'}}
+];
+
+level1Study.eventCodesInfo = makeIntoArray(level1Study.eventCodesInfo);
+var eventData = [];
+for (var i = 0; i < level1Study.eventCodesInfo.length; i++) {
+	level1Study.eventCodesInfo[i].condition = makeIntoArray(level1Study.eventCodesInfo[i].condition);
+	for (var j = 0; j < level1Study.eventCodesInfo[i].condition.length; j++) {
+		eventData.push({
+			code: level1Study.eventCodesInfo[i].code,
+			label: level1Study.eventCodesInfo[i].condition[j].label,
+			description: level1Study.eventCodesInfo[i].condition[j].description,
+			tag: level1Study.eventCodesInfo[i].condition[j].tag
+		});
+	}
+}
+
+
+var lastTimeLevel1EventsFilterChanged = new Date();
+
+function filterLevel1EventsIfNoChange(value) {
+	var thisTime = new Date();
+	if (thisTime.getTime() - lastTimeLevel1EventsFilterChanged.getTime() > 200){
+		extracted.level1EventsGridOptions.api.setQuickFilter(value);
+	}
+}
+
+function onLevelEventsFilterChanged(value) {
+	lastTimeLevel1DatarecordingsFilterChanged = new Date();
+	setTimeout(filterLevel1EventsIfNoChange, 300, value);
+}
+
+extracted.level1EventsGridOptions = {
+	columnDefs: level1EventsColumnDefs,
+	rowData:eventData,
+	enableColResize: true,
+	enableSorting: true,
+	enableFilter: true,
+	suppressMenuHide: true,
+	rowHeight: 60
+};
+
+//license section
+extracted.showLevel1LicensePart = isAvailable(level1Study.summaryInfo.license.link) ||  isAvailable(level1Study.summaryInfo.license.type) ||  isAvailable(level1Study.summaryInfo.license.text);
+extracted.level1LicenseType = level1Study.summaryInfo.license.type;
+extracted.showLevel1LicenseLink = isAvailable(level1Study.summaryInfo.license.link);
+extracted.level1LicenseLink = level1Study.summaryInfo.license.link;
+extracted.showLevel1LicenseText = isAvailable(level1Study.summaryInfo.license.text);
+extracted.level1LicenseText = level1Study.summaryInfo.license.text;
+
+// IRB section
+extracted.showIRBPart = isAvailable(level1Study.irbInfo);
+extracted.IRBtext = level1Study.irbInfo;
+
+// copyright section
+extracted.showLevel1CopyRight = isAvailable(level1Study.copyrightInfo);
+extracted.copyrightText  = level1Study.copyrightInfo;
+
+//---------------------- setting up AngularJS ----------------------------------
 
 angular.module('essReportApp',  ["agGrid"]).controller('ReportController', function($scope) {
 	// transfer key values from extracted to $scope so they are placed in the html template
@@ -255,5 +388,4 @@ angular.module('essReportApp',  ["agGrid"]).controller('ReportController', funct
 		if (!extracted.hasOwnProperty(key)) continue;
 		$scope[key] = extracted[key];
 	}
-	//extracted.gridOptions.api.addEventListener('read', autoSizeAllGridColumns);
 });
