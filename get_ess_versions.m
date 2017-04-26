7a54e31d97d4bb72df0bbcf2087c4782e4d09af4
Nima Bigdely-Shamlo <bigdelys@users.noreply.github.com>
2017-04-26 00:35:37 -0700

    version changed.

--- get_ess_versions.m
+++ get_ess_versions.m
@@ -2,7 +2,7 @@ function [toolsVersion, level1SchemaVersion, level2SchemaVersion, levelDerivedSc
 % [toolsVersion, level1SchemaVersion, level2SchemaVersion, levelDerivedSchemaVersion] = get_ess_versions
 % Returns semver (see semver.org) versions for ESS tools and schemas for different study levels. 
 
-toolsVersion = '2.0.1';
+toolsVersion = '2.1.0';
 level1SchemaVersion = '2.3.1';
 level2SchemaVersion = '1.1.0';
 levelDerivedSchemaVersion = '1.0.0';
